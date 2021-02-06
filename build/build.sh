b8daa8e11f56dac5d5c9161d6b2cf67b0209d916
xulihang <xulihanghai@163.com>
2021-02-06 21:19:32 +0800

    update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-cd BasicCAT/BasicCAT
-export MONO_IOMAP=all
-mono ../../MonoBuilder/B4JBuilder.exe -task=build
-cp Objects/BasicCAT.jar ../../work/BasicCAT/
-cd ../../work/
+cd work/
 wine ../inno/ISCC.exe basiccat.iss
 wine ../inno/ISCC.exe basiccat-x64.iss
+zip -q -r BasicCAT-crossplatforms.zip crossplatform
 
